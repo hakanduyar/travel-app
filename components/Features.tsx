8bf51c5eab17ea6a396d4e3059adbeb0ebcbe0b3
rikords <iamhakanduyar@gmail.com>
2024-01-20 23:04:16 +0300

    Update the Features component to remove the `variant` prop and update the FeatureItem component to use a new markup structure.

--- components/Features.tsx
+++ components/Features.tsx
@@ -33,7 +33,6 @@ const Features = () => {
                   key={feature.title}
                   title={feature.title}
                   icon={feature.icon}
-                  variant={feature.variant}
                   description={feature.description}
                 />
               )),
@@ -48,12 +47,21 @@ const Features = () => {
 type FeatureItem = {
   title: string;
   icon: string;
-  variant: string;
   description: string;
 };
 
-const FeatureItem = ({ title, icon, variant, description }: FeatureItem) => {
-  return <div>{title}</div>;
+const FeatureItem = ({ title, icon, description }: FeatureItem) => {
+  return (
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p7 bg-green-50">
+        <Image src={icon} alt="map" width={28} height={28} />
+      </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
+    </li>
+  );
 };
 
 export default Features;
