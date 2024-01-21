c359d4da07a7ed970dc882639ae61067bf69ce4a
rikords <iamhakanduyar@gmail.com>
2024-01-21 14:30:04 +0300

    Update Features component styles and markup.

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Features = () => {
   return (
-    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full justify-end">
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -26,7 +26,7 @@ const Features = () => {
             />
             <h2 className="bold-40 lg:bold-64 ">Our Features</h2>
           </div>
-          <ul>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:mg-20">
             {[
               FEATURES.map((feature) => (
                 <FeatureItem
@@ -57,7 +57,7 @@ const FeatureItem = ({ title, icon, description }: FeatureItem) => {
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
       <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
-      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-nonex">
         {description}
       </p>
     </li>
